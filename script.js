987cb5ace2d0c48cce01e261f3a87747c48a7a22
Odin <odinabasto97@gmail.com>
2023-06-02 00:01:46 +0200

    anaidendo elementos

---
+++ script.js
@@ -0,0 +1,25 @@
+function appendSymbol(symbol) {
+    var result = document.getElementById("result");
+    result.value += symbol;
+  }
+  
+  function clearResult() {
+    var result = document.getElementById("result");
+    result.value = "";
+  }
+  
+  function deleteDigit() {
+    var result = document.getElementById("result");
+    result.value = result.value.slice(0, -1);
+  }
+  
+  function calculate() {
+    var result = document.getElementById("result");
+    var expression = result.value;
+  
+    try {
+      result.value = eval(expression);
+    } catch (error) {
+      result.value = "Error";
+    }
+  }
